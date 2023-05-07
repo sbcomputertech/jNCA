e9f88cf3a148df300644a329c4afff0199c54d0e
sbcomputertech <sbcomputertech.0@gmail.com>
2023-05-07 17:35:20 +0100

    fixed flickering yay

--- src/main/java/com/calliefox/jnca/ui/JncaApplication.java
+++ src/main/java/com/calliefox/jnca/ui/JncaApplication.java
@@ -20,20 +20,21 @@ public class JncaApplication extends JFrame {
         simTimer.scheduleAtFixedRate(new SimTimerTask(cellGridManager, this), 0, 500);
 
         cellGridManager.cells[50][50].value = 1;
-
+        add(new GridPanel());
         setVisible(true);
     }
 
-    @Override
-    public void paint(Graphics g) {
-        super.paint(g);
-        Graphics2D g2d = (Graphics2D) g;
-        int size = 5;
-        new CellIterWorker(cellGridManager, (x, y, p) -> {
-            float v = p.cells[x][y].value;
-            Color c = Utils.lerpColor(Color.BLACK, Color.MAGENTA, v);
-            g2d.setColor(c);
-            g2d.fillRect(x * size, y * size, size, size);
-        }).execute();
+    public class GridPanel extends JComponent {
+        @Override
+        public void paintComponent(Graphics g) {
+            Graphics2D g2d = (Graphics2D) g;
+            int size = 5;
+            new CellIterWorker(cellGridManager, (x, y, p) -> {
+                float v = p.cells[x][y].value;
+                Color c = Utils.lerpColor(Color.BLACK, Color.MAGENTA, v);
+                g2d.setColor(c);
+                g2d.fillRect(x * size, y * size, size, size);
+            }).execute();
+        }
     }
 }
